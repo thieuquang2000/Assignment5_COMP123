181825f6fb99568421821faf1d2b11364289232c
thieuquang2000 <52172346+thieuquang2000@users.noreply.github.com>
2019-07-25 12:20:59 -0400

    Add files via upload

---
+++ COMP123-S2019-Lesson9B-master/COMP123-S2019-Lesson9B/Animation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment5
+{
+   public enum Animation
+    {
+        IDLE,
+        UP,
+        DOWN,
+        NUM_OF_STATES
+    }
+}
